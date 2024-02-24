2cf7902128cb3f0d604a43c73c61fe53c3f61767
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:42:34 +0500

    Add files via upload

---
+++ q12.js
@@ -0,0 +1,9 @@
+"use strict";
+/* Ques # 12:
+Greetings: start with the array you used in the exercise 11 but instead of just printing each person's name, print a message to them.The text of each message should be the same , but each message should be personalized with person's name.*/
+let names = ["Ali", "Hussain", "Zohaib", "Sohail"];
+let message = "How are you Dear?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
---
+++ q12.ts
@@ -0,0 +1,11 @@
+/* Ques # 12:
+Greetings: start with the array you used in the exercise 11 but instead of just printing each person's name, print a message to them.The text of each message should be the same , but each message should be personalized with person's name.*/
+
+let names :string[] = ["Ali","Hussain","Zohaib","Sohail"]
+
+let message :string ="How are you Dear?"
+
+console.log(names[0] +" " + message );
+console.log(names[1] +" " + message );
+console.log(names[2] +" " + message );
+console.log(names[3] +" " + message );
